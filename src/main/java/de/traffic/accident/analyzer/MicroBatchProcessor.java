a8292f35e5dc41f891b3d374061cdcefc2eb3556
Kipka <jan.kipka@hpe.com>
2017-07-31 19:08:49 +0200

    pom anpassung

--- src/main/java/de/traffic/accident/analyzer/MicroBatchProcessor.java
+++ src/main/java/de/traffic/accident/analyzer/MicroBatchProcessor.java
@@ -1,9 +1,65 @@
 package de.traffic.accident.analyzer;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.streaming.Durations;
+import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.streaming.api.java.JavaPairDStream;
+import org.apache.spark.streaming.api.java.JavaPairInputDStream;
+import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import org.apache.spark.streaming.kafka.KafkaUtils;
+
+import com.google.common.collect.Lists;
+
+import kafka.serializer.StringDecoder;
+import scala.Tuple2;
+
 public class MicroBatchProcessor {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		SparkConf sparkContext = new SparkConf().setAppName("TrafficAccidentAnalyzer").setMaster("local[2]");
+		JavaStreamingContext streamingCont = new JavaStreamingContext(sparkContext, Durations.seconds(10));
+		
+		HashMap<String, String> kafkaParams = new HashMap<>();
+		//BOOTSTRAP SCHREIBEN
+		kafkaParams.put("bootstrap.servers", "uoit.farberg.de:9092");
+		//ZOOKEEPER AUSLESEN
+		kafkaParams.put("zookeeper.connect", "uoit.farberg.de:2181");
+		//kafkaParams.put("key.deserializer", StringDeserializer.class);
+		//kafkaParams.put("value.deserializer", StringDeserializer.class);
+		kafkaParams.put("group.id", "taa_mbprocessor_1");
+		
+		Set<String> topics = new HashSet<>();
+		topics.add("accidents");
+		JavaPairInputDStream<String, String> lines = 
+			     KafkaUtils.createDirectStream(streamingCont,
+			    		 String.class, String.class,StringDecoder.class, StringDecoder.class,
+			        kafkaParams,topics);
+		
+		JavaDStream<String> words = lines.flatMap(x -> Lists.newArrayList(x._2.split(" ")));
+		
+		words.print();
+		
+//		JavaPairDStream<String, Integer> wordCounts = words.mapToPair(word -> new Tuple2<String, Integer>(word, 1))
+//				.reduceByKey((i1, i2) -> i1 + i2);
+//		
+//		wordCounts.print();
+		
+		streamingCont.start();
+
+		streamingCont.awaitTermination();
+		streamingCont.close();
+		
+//		JavaReceiverInputDStream<String> lines = streamingCont.socketTextStream("localhost", 9092);
+//		JavaDStream<String> words = lines.flatMap(x -> Arrays.asList(x.split(" ")).iterator());
+		
+		
 
 	}
 
