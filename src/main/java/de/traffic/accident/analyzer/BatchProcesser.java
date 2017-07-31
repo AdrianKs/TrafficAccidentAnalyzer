d65313d411700473499fe89edc3b2a9531f66129
Adrian Kochsiek <Adrian Kochsiek>
2017-07-31 14:54:11 -0400

    some changes in batchprocessing

--- src/main/java/de/traffic/accident/analyzer/BatchProcesser.java
+++ src/main/java/de/traffic/accident/analyzer/BatchProcesser.java
@@ -13,49 +13,33 @@ import com.google.gson.GsonBuilder;
 import de.uniluebeck.itm.util.logging.Logging;
 import scala.Tuple2;
 
-public class BatchProcesser {
+public class BatchProcesser implements Runnable {
 	
 	public static JavaPairRDD<String, Integer> reduceByBrand;
 	public static JavaPairRDD<Integer, Integer> reduceByYear;
 	public static String JsonByBrand;
+	private DataFrame df = null;
+	private SparkConf conf = null;
+	private JavaSparkContext sc = null;
+	private SQLContext sqlContext = null;
 	
 	
 	public BatchProcesser() {
-		createData();
-	}
-	
-	public static String getJsonByBrand(){
-		if(JsonByBrand != null) {
-			return JsonByBrand;
-		}
-		else {
-			return "Seems to be Null";
-		}
-		
-	}
-	
-	public static void main(String[] args) {
-		createData();
-	}
-	
-	public static void createData(){
-		Logging.setLoggingDefaults();
-		String fileName = "src/main/resources/sumo-sim-out.csv";
-		// String fileName = "src/main/resources/test.csv"; //Should return 10 for vehicle id 0
+				Logging.setLoggingDefaults();
+		this.conf = new SparkConf().setMaster("local[*]").setAppName("Batch Processor");
+		this.sc = new JavaSparkContext(conf);
 
-		SparkConf conf = new SparkConf().setMaster("local[*]").setAppName("Batch Processor");
-		JavaSparkContext sc = new JavaSparkContext(conf);
-
-		SQLContext sqlContext = new SQLContext(sc);
+		this.sqlContext = new SQLContext(sc);
 
 		// Insert code here
-		DataFrame df = sqlContext.read()
+		this.df = sqlContext.read()
 			    .format("com.databricks.spark.csv")
 			    .option("inferSchema", "true")
 			    .option("header", "true")
 			    .load("src/main/resources/taa.csv");
-		
-		
+	}
+	
+	public void run() {
 		//Analyze number of accidents per brand
 		JavaRDD<Row> javaRDD = df.javaRDD();
 		JavaPairRDD<String, Integer> brandMap = javaRDD.mapToPair(row -> new Tuple2<>(row.getString(0), 1));
@@ -71,5 +55,16 @@ public class BatchProcesser {
         
         sc.close();
 	}
-
+	
+	public static String getJsonByBrand(){
+		if(JsonByBrand != null) {
+			return JsonByBrand;
+		}
+		else {
+			return "Seems to be Null";
+		}
+		
+	}
+	
+	
 }
