426d2dc6fbde25e994eb48db37c6eb437b488066
xqtion <andre-h96@gmx.de>
2017-08-01 22:56:40 +0200

    Statistiken zur Web UI hinzugefügt

--- src/main/java/de/traffic/accident/analyzer/SparkWebserver.java
+++ src/main/java/de/traffic/accident/analyzer/SparkWebserver.java
@@ -21,9 +21,14 @@ public class SparkWebserver {
 		 
 		 get("/index", (request, response) -> {
 			 	JsonElement numbAccidentsToBrand = Database.getNumbAccidentsToBrand();
+			 	JsonElement numbAccidentsToYearOfCar = Database.getNumbAccidentsToYearOfCar();
+			 	JsonElement numbAccidentsToNumbPasseger = Database.getNumbAccidentsToNumbPasseger();
+			 	JsonElement numbOfDiffAccidentType = Database.getNumbOfDiffAccidentType();
 	            Map<String, Object> model = new HashMap<>();
 	            model.put("numbAccidentsToBrand", numbAccidentsToBrand);
-	            
+	            model.put("numbAccidentsToYearOfCar", numbAccidentsToYearOfCar);
+	            model.put("numbAccidentsToNumbPasseger", numbAccidentsToNumbPasseger);
+	            model.put("numbOfDiffAccidentType", numbOfDiffAccidentType);
 	            
 	            return new ModelAndView(model, "public/index.vm");
 	        }, new VelocityTemplateEngine());
